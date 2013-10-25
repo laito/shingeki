dec0cb598377a5c1a6889ba89bbcdd8380aae5c0
Apoorv Singh <xnotifysend@gmail.com>
2013-10-26 00:45:42 +0530

    SSL cert

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Shingeki
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
+   config.assets.enabled = false 
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
  Rails.application.config.middleware.use OmniAuth::Builder do
    provider :google_oauth2, "83003395981-d5so1015ogdlrhbg3mtappc91nh3hc56.apps.googleusercontent.com", "5H0A8affzk11pc2f5grNSn4P", {
      access_type: 'offline',
-    client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}},
+    #client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}},
+    client_options: {ssl: { :verify => false}},
      scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
      redirect_uri:'http://localhost/auth/google_oauth2/callback'
    }
