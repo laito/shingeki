c6a7fab81bfd638f33477cd5127316c8fa1d2126
Apoorv Singh <xnotifysend@gmail.com>
2013-10-26 02:36:03 +0530

    Merge branch 'master' of https://github.com/laito/shingeki
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- app/controllers/api/signins_controller.rb
+++ app/controllers/api/signins_controller.rb
@@ -10,7 +10,7 @@ require 'net/http'
 		req = Net::HTTP::Get.new(URI.encode(url.to_s))
 		http = Net::HTTP.new(url.host, url.port)
 		http.use_ssl = true
-		http.verify_mode = nil
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 		#https.ca_file = "lib/assets/cacert.pm"
 		resp = nil
 		data = nil
